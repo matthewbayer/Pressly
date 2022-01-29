b94efe8cd17a066332103049566d68ac41262582
Matt Bayer <meb287@cornell.edu>
2022-01-29 02:06:27 -0500

    update procfile 5

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -180,31 +180,15 @@ DATABASES['default'].update(db_from_env)
 
 RQ_QUEUES = {
     'default': {
-        'HOST': 'localhost',
-        'PORT': 6379,
-        'DB': 0,
         'PASSWORD': 'presslyredisftw',
         'DEFAULT_TIMEOUT': 360,
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
-    },
-    'with-sentinel': {
-        'SENTINELS': [('localhost', 26736), ('localhost', 26737)],
-        'MASTER_NAME': 'redismaster',
-        'DB': 0,
-        'PASSWORD': 'secret',
-        'SOCKET_TIMEOUT': None,
-        'CONNECTION_KWARGS': {
-            'socket_connect_timeout': 0.3
-        },
     },
     'high': {
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
         'DEFAULT_TIMEOUT': 500,
     },
     'low': {
-        'HOST': 'localhost',
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
-        'PORT': 6379,
-        'DB': 0,
     }
 }
