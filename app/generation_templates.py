230856d07807cac21fe7e22bf992a910ab34b1ad
Matt Bayer <meb287@cornell.edu>
2022-05-07 15:09:57 -0400

    update prompt

--- app/generation_templates.py
+++ app/generation_templates.py
@@ -11,4 +11,40 @@ FOR IMMEDIATE RELEASE
 
 {location}, {release_date} /PRNewswire/ -- Today,"""
 
-cow_template = """*****\nCow 914\nPrevious activities: Pacing around, laying down.\nActions needed: None\n*****\n\n*****\nCow 914\nPrevious activities: Limping\nActions needed: Medical check-up\n*****\n\n*****\nCow 673\nPrevious activities: Drinking water.\nActions needed: Refill water\n*****\n\n*****\nCow 1253\nPrevious activities: Swollen left back leg\nActions needed: Administer antibiotics\n*****\n\n*****\nCow 124\nPrevious activities: Eating hay, mooing, mating.\nActions needed: Food refill, Pregnancy test.\n*****\n\n*****\nCow 192\nPrevious activities: Vomiting, falling unconscious.\nActions needed: Medical check-up\n*****\n\n*****\nCow 289\nPrevious activities: Aggressive, charging other cows.\nActions needed: Restrain cow and administer tranquilizer\n*****\n\n*****\nCow 1253\nPrevious activities: Sleeping\nActions needed: None\n*****\n\n*****\nCow 1253\nPrevious activities: Giving birth\nActions needed: Emergency medical checkup\n*****\n\n*****\nCow 314\nPrevious activities: {iot}\nActions needed:"""
+#cow_template = """*****\nCow 914\nPrevious activities: Pacing around, laying down.\nActions needed: None\n*****\n\n*****\nCow 914\nPrevious activities: Limping\nActions needed: Medical check-up\n*****\n\n*****\nCow 673\nPrevious activities: Drinking water.\nActions needed: Refill water\n*****\n\n*****\nCow 1253\nPrevious activities: Swollen left back leg\nActions needed: Administer antibiotics\n*****\n\n*****\nCow 124\nPrevious activities: Eating hay, mooing, mating.\nActions needed: Food refill, Pregnancy test.\n*****\n\n*****\nCow 192\nPrevious activities: Vomiting, falling unconscious.\nActions needed: Medical check-up\n*****\n\n*****\nCow 289\nPrevious activities: Aggressive, charging other cows.\nActions needed: Restrain cow and administer tranquilizer\n*****\n\n*****\nCow 1253\nPrevious activities: Sleeping\nActions needed: None\n*****\n\n*****\nCow 4896\nPrevious activities: Giving birth\nActions needed: Emergency medical checkup\n*****\n\n*****\nCow 1253\nPrevious activities: Giving birth\nActions needed: Emergency medical checkup\n*****\n\n*****\nCow 314\nPrevious activities: {iot}\nActions needed:"""
+
+cow_template = """1. Cow status: Pacing around, laying down.
+Recommendation for cow: None
+
+2. Cow status: Limping
+Recommendation for cow: Medical check-up
+
+3. Cow status: Drinking water.
+Recommendation for cow: Refill water
+    
+4. Cow status: Swollen left back leg
+Recommendation for cow: Administer antibiotics
+    
+5. Cow status: Eating hay, mooing.
+Recommendation for cow: Food refill
+    
+6. Cow status: Vomiting, falling unconscious.
+Recommendation for cow: Medical check-up
+    
+7. Cow status: Aggressive, charging other cows.
+Recommendation for cow: Restrain cow and administer tranquilizer
+
+8. Cow status: Sleeping
+Recommendation for cow: None
+    
+9. Cow status: Giving birth
+Recommendation for cow: Emergency medical checkup
+
+10. Cow status: Mating.
+Recommendation for cow: Pregnancy test.
+
+11. Cow status: {iot}
+Recommendation for cow:"""
+
+
+
